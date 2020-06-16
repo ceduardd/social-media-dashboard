6c76cf8f7240d6b75b6e12af418fcea8930e20a6
Eduardo Chávez <eduardochavez_99@outlook.com>
2020-06-16 16:19:52 -0500

    code refactoring ⚙

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 const chk = document.getElementById('chk')
 
 const label = document.querySelector('.label')
-const cards = document.querySelectorAll('.card')
 const header = document.querySelector('header')
 const body = document.body
 
 const h1 = document.querySelector('h1')
 const h2 = document.querySelector('h2')
 
-const fwNumbers = document.querySelectorAll('.followers-number')
-const metricNumbers = document.querySelectorAll('.metric-number')
+const cards = document.querySelectorAll('.card')
+const numbers = document.querySelectorAll('[class$="number"]')
 
-console.log(h1)
-console.log(h2)
-
-console.log(body)
+console.log(numbers)
 
 chk.addEventListener('change', () => {
   label.classList.toggle('label-dark')
@@ -27,11 +23,7 @@ chk.addEventListener('change', () => {
     cards[i].classList.toggle('card-dark')
   }
 
-  for (let i = 0; i < fwNumbers.length; i++) {
-    fwNumbers[i].classList.toggle('white-text')
-  }
-
-  for (let i = 0; i < metricNumbers.length; i++) {
-    metricNumbers[i].classList.toggle('white-text')
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].classList.toggle('white-text')
   }
 })
